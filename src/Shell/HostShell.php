2468502906fdbf9df9983817a3aa0cd51c15e2c2
Walther Lalk <waltherlalk@gmail.com>
2015-05-08 09:38:50 +0200

    Clean up hosts shell and fix issue with concurrent jobs running

--- src/Shell/HostShell.php
+++ src/Shell/HostShell.php
@@ -12,6 +12,7 @@ class HostShell extends Shell
     public $modelClass = 'DelayedJobs.DelayedJobs';
     protected $_workerId;
     protected $_workerName;
+    protected $_runningJobs = [];
 
     public function main()
     {
@@ -26,7 +27,7 @@ class HostShell extends Shell
             $this->_stop(1);
         }
 
-        $this->_workerId = $this->_workerName . ' - ' . php_uname('a');
+        $this->_workerId = $this->_workerName . ' - ' . php_uname('n');
 
         /*
          * Get Next Job
@@ -36,34 +37,32 @@ class HostShell extends Shell
          * Worker monitors the exection time
          */
 
-        $job_pids = [];
-
-        $max_allowed_jobs = 1;
+        $max_allowed_jobs = $this->param('maxJobs');
 
         //## Need to make sure that any running jobs for this host is in the array job_pids
 
-        $running_jobs = $this->DelayedJobs->getRunningByHost($this->_workerId);
-
-        foreach ($running_jobs as $running_job) {
-            $job_pids[$running_job->id] = [
-                'pid' => $running_job->pid,
+        $db_jobs = $this->DelayedJobs
+            ->getRunningByHost($this->_workerId)
+            ->toArray();
+        foreach ($db_jobs as $running_job) {
+            $this->_runningJobs[$running_job->id] = [
+                'pid' => $running_job->pid
             ];
         }
 
-        $this->out('<info>Started up</info>', 1, Shell::VERBOSE);
+        $this->out(sprintf('<info>Started up:</info> %s', $this->_workerId), 1, Shell::VERBOSE);
         while (true) {
-            if (count($job_pids) < $max_allowed_jobs) {
+            $number_jobs = count($this->_runningJobs);
+            if ($number_jobs < $max_allowed_jobs) {
                 $this->_executeJob();
-            } else {
-                $this->out('<info>Maximum allowed jobs running</info>', 1, Shell::VERBOSE);
             }
 
             //## Check Status of Fired Jobs
-            foreach ($job_pids as $job_id => $running_jobs) {
+            foreach ($this->_runningJobs as $job_id => $running_job) {
                 $job = $this->DelayedJobs->get($job_id);
 
                 $status = new Process();
-                $status->setPid($running_jobs['pid']);
+                $status->setPid($running_job['pid']);
                 if (!$status->status()) {
                     //## Make sure that this job is not marked as running
                     if ($job->status === DelayedJobsTable::STATUS_BUSY) {
@@ -72,23 +71,20 @@ class HostShell extends Shell
                             'Job not running, but db said it is, could be a runtime error'
                         );
                     }
-                    unset($job_pids[$job_id]);
-                    $this->out('<info>Job: ' . $job_id . ' no longer running</info>', 1, Shell::VERBOSE);
+                    $time = time() - $running_job['start_time'];
+                    unset($this->_runningJobs[$job_id]);
+                    $this->out(sprintf('<success>Job\'s done:</success> %s, took %d seconds', $job_id, $time), 2, Shell::VERBOSE);
                 } else {
                     //## Check if job has not reached it max exec time
-                    $busy_time = time() - $running_jobs['start_time'];
+                    $busy_time = time() - $running_job['start_time'];
 
-                    if ($busy_time > $running_jobs['max_execution_time']) {
-                        $this->out('<info>Job: ' . $job_id . ' Running too long, need to kill it</info>', 1, Shell::VERBOSE);
+                    if ($busy_time > $running_job['max_execution_time']) {
+                        $this->out(sprintf('<error>Job timeout:</error> %s', $job_id), 2, Shell::VERBOSE);
                         $status->stop();
 
                         $this->DelayedJobs->failed($job, 'Job ran too long, killed');
                     } else {
-                        $this->out(
-                            '<info>Job: ' . $job_id . ' still running: ' . $busy_time . '</info>',
-                            1,
-                            Shell::VERBOSE
-                        );
+                        $this->out(sprintf('<info>Still running:</info> %s, %d seconds', $job_id, $busy_time), 1, Shell::VERBOSE);
                     }
                 }
             }
@@ -102,8 +98,8 @@ class HostShell extends Shell
         $job = $this->DelayedJobs->getOpenJob($this->_workerId);
 
         if ($job) {
-            $this->out('<info>Got a new job: ' . $job->id . '</info>', 1, Shell::VERBOSE);
-            if (!isset($job_pids[$job->id])) {
+            $this->out(sprintf('<success>Starting job:</success> %s', $job->id), 1, Shell::VERBOSE);
+            if (!isset($this->_runningJobs[$job->id])) {
                 $options = unserialize($job->options);
 
                 if (!isset($options['max_execution_time'])) {
@@ -117,12 +113,12 @@ class HostShell extends Shell
 
                 $this->DelayedJobs->setPid($job, $pid);
 
-                $job_pids[$job->id] = [
+                $this->_runningJobs[$job->id] = [
                     'pid' => $pid,
                     'start_time' => time(),
                     'max_execution_time' => $options['max_execution_time'],
                 ];
-                $this->out('<info>Job runner forked</info>', 1, Shell::VERBOSE);
+                $this->out(' - <info>Runner started</info>', 1, Shell::VERBOSE);
             }
         }
     }
@@ -132,6 +128,14 @@ class HostShell extends Shell
         $options = parent::getOptionParser();
 
         $options
+            ->addOption(
+                'maxJobs',
+                [
+                    'help' => 'Number of jobs to run concurrently',
+                    'short' => 'n',
+                    'default' => 1
+                ]
+            )
             ->addArgument('workerName', [
                 'help' => 'Custom worker name to use',
             ]);
