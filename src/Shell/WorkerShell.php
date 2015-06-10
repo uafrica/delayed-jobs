0c056359104a8ab86ecd8cf31adc58f9d9173543
Walther Lalk <waltherlalk@gmail.com>
2015-06-10 14:27:27 +0200

    Add ability to manually force a job to re-run

--- src/Shell/WorkerShell.php
+++ src/Shell/WorkerShell.php
@@ -50,23 +50,25 @@ class WorkerShell extends Shell
 
         try {
             $job = $this->DelayedJobs->get($job_id);
+            $this->out(' - Got job from DB', 1, Shell::VERBOSE);
         } catch (RecordNotFoundException $e) {
             $this->out('<fail>Job ' . $job_id . ' not found (' . $e->getMessage() . ')</fail>');
             $this->_stop(1);
             return;
         }
+        //## First check if job is not locked
+        if (!$this->param('force') && $job->status == DelayedJobsTable::STATUS_SUCCESS) {
+            throw new Exception("Job previously completed, Why is is being called");
+        }
+
+        if (!$this->param('force') && $job->status == DelayedJobsTable::STATUS_BURRIED) {
+            throw new Exception("Job Failed too many times, but why was it called again");
+        }
 
         try {
-            //## First check if job is not locked
-            if ($job->status == DelayedJobsTable::STATUS_SUCCESS) {
-                throw new Exception("Job previously completed, Why is is being called");
-            }
-
-            if ($job->status == DelayedJobsTable::STATUS_BURRIED) {
-                throw new Exception("Job Failed too many times, but why was it called again");
-            }
-
+            $this->out(' - Executing job', 1, Shell::VERBOSE);
             $response = $job->execute();
+            $this->out(' - Execution complete', 1, Shell::VERBOSE);
 
             if ($response) {
                 $this->DelayedJobs->completed($job, is_string($response) ? $response : null);
@@ -92,6 +94,11 @@ class WorkerShell extends Shell
         $options = parent::getOptionParser();
 
         $options
+            ->addOption('force', [
+                'short' => 'f',
+                'help' => 'Force the job to run, even if failed, or successful',
+                'boolean' => true
+            ])
             ->addArgument(
                 'jobId',
                 [
