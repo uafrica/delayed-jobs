5216a41d5c58060d6791621969c3fa30ddffa088
Walther Lalk <waltherlalk@gmail.com>
2015-09-08 13:55:12 +0200

    Better colours

--- src/Shell/MonitorShell.php
+++ src/Shell/MonitorShell.php
@@ -190,15 +190,15 @@ class MonitorShell extends Shell
             }
             $this->hr();
             if ($last_failed) {
-                $this->out(__('<info>{0} ({1}::{2}())</info> failed because <info>{3}</info> at <info>{4}</info>', $last_failed->id, $last_failed->class, $last_failed->method,
+                $this->out(__('<info>{0}</info> <comment>{1}::{2}()</comment> failed because <info>{3}</info> at <info>{4}</info>', $last_failed->id, $last_failed->class, $last_failed->method,
                     $last_failed->last_message, $last_failed->failed_at->i18nFormat()));
             }
             if ($last_buried) {
-                $this->out(__('<info>{0} ({1}::{2}())</info> was buried because <info>{3}</info> at <info>{4}</info>',
+                $this->out(__('<info>{0}</info> <comment>{1}::{2}()</comment> was buried because <info>{3}</info> at <info>{4}</info>',
                     $last_buried->id, $last_buried->class, $last_buried->method, $last_buried->last_message, $last_buried->failed_at->i18nFormat()));
             }
             if ($longest_running) {
-                $this->out(__('<info>{0} ({1}::{2}())</info> is the longest running job at <info>{3}</info>', $longest_running->id,
+                $this->out(__('<info>{0}</info> <comment>{1}::{2}()</comment> is the longest running job at <info>{3}</info>', $longest_running->id,
                     $longest_running->class, $longest_running->method, $longest_running->diff));
             }
             if ($this->param('snapshot')) {
