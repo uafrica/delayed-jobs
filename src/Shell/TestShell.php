bf5f5be75c1f9f2bf5049f329ad70d1aaa8b2035
Walther Lalk <waltherlalk@gmail.com>
2017-08-11 15:51:57 +0200

    Add shell to queue sequenced jobs

--- src/Shell/TestShell.php
+++ src/Shell/TestShell.php
@@ -2,6 +2,7 @@
 namespace DelayedJobs\Shell;
 
 use App\Shell\AppShell;
+use Cake\Console\ConsoleOptionParser;
 use Cake\Console\Shell;
 use DelayedJobs\DelayedJob\EnqueueTrait;
 use DelayedJobs\DelayedJob\Job;
@@ -17,6 +18,34 @@ class TestShell extends AppShell
     use EnqueueTrait;
     
     public $modelClass = 'DelayedJobs.DelayedJobs';
+
+    public function sequencing()
+    {
+        $this->out('<info>Creating 10 jobs with sequencing, 3 will fail</info>');
+
+        $rand = random_int(0, time());
+        $failures = [
+            1, 5, 6
+        ];
+        for ($i = 0; $i < 10; $i++) {
+            $this->enqueue(
+                'DelayedJobs.Test',
+                ['type' => in_array($i, $failures) ? 'fail' : 'success'],
+                [
+                    'maxRetries' => 2,
+                    'sequence' => 'TestJob::' . $rand
+                ]
+            );
+            sleep(1);
+            if (in_array($i, $failures)) {
+                $this->out('<error>' . $i . '</error>');
+            } else {
+                $this->out('<success>' . $i . '</success>');
+            }
+        }
+
+        $this->out('<success>All queued. Check the table</success>');
+    }
 
     /**
      * @return bool
@@ -70,5 +99,19 @@ class TestShell extends AppShell
         $this->out('<info>All Done</info>', 1, Shell::VERBOSE);
 
         return true;
+    }
+
+    /**
+     * @return \Cake\Console\ConsoleOptionParser
+     */
+    public function getOptionParser(): ConsoleOptionParser
+    {
+        $options = parent::getOptionParser();
+
+        $options->addSubcommand('sequencing', [
+                'help' => 'Sequencing'
+            ]);
+
+        return $options;
     }
 }
