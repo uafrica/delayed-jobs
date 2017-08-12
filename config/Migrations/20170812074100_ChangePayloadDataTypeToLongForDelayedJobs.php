11580ec1926566641d38c41dc15dd51bacb3c4f4
Walther Lalk <waltherlalk@gmail.com>
2017-08-12 09:32:44 +0200

    Fix classname

--- config/Migrations/20170812074100_ChangePayloadDataTypeToLongForDelayedJobs.php
+++ config/Migrations/20170812074100_ChangePayloadDataTypeToLongForDelayedJobs.php
@@ -1,7 +1,7 @@
 <?php
 use Migrations\AbstractMigration;
 
-class ChangePayloadDataTypeForDelayedJobs extends AbstractMigration
+class ChangePayloadDataTypeToLongForDelayedJobs extends AbstractMigration
 {
     /**
      * Change Method.
