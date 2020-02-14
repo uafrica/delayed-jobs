66047e9abeb4fbd819540b3ec70f915a55f88462
Walther Lalk <dakota@users.noreply.github.com>
2020-02-14 12:37:45 +0200

    Use correct phpunit constraints

---
+++ src/TestSuite/Constraint/JobConstraintBase.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace DelayedJobs\TestSuite\Constraint;
+
+use Cake\TestSuite\TestEmailTransport;
+use DelayedJobs\TestSuite\TestManager;
+use PHPUnit\Framework\Constraint\Constraint;
+
+/**
+ * Base class for all mail assertion constraints
+ *
+ * @internal
+ */
+abstract class JobConstraintBase extends Constraint
+{
+    /**
+     * @var int|null
+     */
+    protected $at;
+
+    /**
+     * Constructor
+     *
+     * @param int|null $at At
+     * @return void
+     */
+    public function __construct(?int $at = null)
+    {
+        $this->at = $at;
+    }
+
+    /**
+     * Gets the jobs to check
+     *
+     * @return \DelayedJobs\DelayedJob\Job[]
+     */
+    public function getJobs()
+    {
+        $jobs = TestManager::getJobs();
+
+        if ($this->at) {
+            if (!isset($jobs[$this->at])) {
+                return [];
+            }
+
+            return [$jobs[$this->at]];
+        }
+
+        return $jobs;
+    }
+}
