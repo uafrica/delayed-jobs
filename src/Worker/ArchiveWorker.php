02e3245be164838c6d0e9452d7dd3dce39370174
Walther Lalk <dakota@users.noreply.github.com>
2020-09-16 14:37:11 +0200

    Paginate archive job queries

--- src/Worker/ArchiveWorker.php
+++ src/Worker/ArchiveWorker.php
@@ -11,9 +11,12 @@ use Cake\Log\Log;
 use Cake\ORM\Table;
 use Cake\ORM\TableRegistry;
 use DelayedJobs\DelayedJob\Job;
+use Lampager\Cake\ORM\Query;
 
 /**
  * Class ArchiveWorker
+ * @property \DelayedJobs\Model\Table\DelayedJobsTable $DelayedJobs
+ * @property \Cake\ORM\Table $Archive
  */
 class ArchiveWorker extends Worker
 {
@@ -45,6 +48,95 @@ class ArchiveWorker extends Worker
     }
 
     /**
+     * @param \Cake\ORM\Query $baseQuery Base query to paginate
+     * @param int|null $total Total number of records
+     * @return \Generator
+     */
+    protected function doCursorPagination(\Cake\ORM\Query $baseQuery, ?int $total = null): \Generator
+    {
+        if (!class_exists('Lampager\Cake\Datasource\Paginator')) {
+            yield $baseQuery;
+
+            return;
+        }
+
+        $perPage = Configure::read('DelayedJobs.archive.perPage', 1000);
+        $baseQuery->limit($perPage);
+        $pagedQuery = Query::fromQuery($baseQuery);
+        $pagedQuery->unseekable(true);
+
+        $cursor = [];
+        $hasNext = true;
+
+        Log::debug('Using pagination.');
+        if ($total) {
+            Log::debug('Total of ' . ceil($total / $perPage) . ' pages.');
+        }
+
+        while ($hasNext) {
+            usleep(100000); // Wait 100ms per page to not kill the DBS
+            $pagedQuery->cursor($cursor);
+            $pagedResult = $pagedQuery->all();
+
+            $hasNext = $pagedResult->hasNext;
+            $cursor = $pagedResult->nextCursor;
+
+            yield $pagedQuery;
+        }
+    }
+
+    /**
+     * @return \Generator|void
+     */
+    protected function getJobsToArchive(): \Generator
+    {
+        $baseQuery = $this->DelayedJobs->query()
+            ->where(['status IN' => [Job::STATUS_BURIED, Job::STATUS_SUCCESS]])
+            ->order(['id' => 'ASC']);
+
+        $total = $baseQuery->count();
+        if ($total === 0) {
+            Log::debug('No jobs to be archived.');
+
+            return;
+        }
+
+        Log::debug($total . ' jobs to be archived.');
+
+        yield from $this->doCursorPagination($baseQuery, $total);
+    }
+
+    /**
+     * @param \Cake\I18n\Time $time Time up to which to delete
+     * @return \Generator|void
+     */
+    protected function getJobsToDelete(Time $time): \Generator
+    {
+        $lastIdToDelete = $this->Archive->find()
+                ->select(['id'])
+                ->where(['created <=' => $time])
+                ->orderDesc('id')
+                ->disableHydration()
+                ->first()['id'] ?? null;
+
+        if (!$lastIdToDelete) {
+            Log::debug('No jobs to be deleted.');
+
+            return;
+        }
+
+        $baseQuery = $this->Archive->query()
+            ->select(['id'])
+            ->where([
+                'id <=' => $lastIdToDelete,
+                'created <=' => $time,
+            ])
+            ->order(['id' => 'ASC']);
+
+        yield from $this->doCursorPagination($baseQuery);
+    }
+
+    /**
      * @param \DelayedJobs\DelayedJob\Job $job The job that is being run.
      * @return null|bool|\Cake\I18n\Time|string
      */
@@ -54,40 +146,55 @@ class ArchiveWorker extends Worker
             return 'Not enabled for archiving';
         }
 
-        $delayedJobsTable = TableRegistry::getTableLocator()->get('DelayedJobs.DelayedJobs');
-        $archiveTable = TableRegistry::getTableLocator()->get('Archive', [
+        $this->loadModel('DelayedJobs.DelayedJobs');
+        $this->Archive = TableRegistry::getTableLocator()->get('Archive', [
             'table' => Configure::read('DelayedJobs.archive.tableName'),
         ]);
 
-        $this->_ensureTable($archiveTable);
+        $this->_ensureTable($this->Archive);
 
-        $connection = $archiveTable->getConnection();
+        $connection = $this->Archive->getConnection();
         $quote = $connection->getDriver()
             ->isAutoQuotingEnabled();
         $connection->getDriver()
             ->enableAutoQuoting(true);
 
-        $selectQuery = $delayedJobsTable->query()
-            ->where(['status IN' => [Job::STATUS_BURIED, Job::STATUS_SUCCESS]]);
+        $columns = $this->DelayedJobs->getSchema()
+            ->columns();
+        $pageNumber = 1;
+        foreach ($this->getJobsToArchive() as $toArchiveQuery) {
+            Log::debug('Archiving page ' . $pageNumber++);
+            $clonedQuery = clone $toArchiveQuery;
 
-        Log::debug($selectQuery->count() . ' jobs to be archived.');
-        $insertQuery = $archiveTable->query();
-        $insertQuery
-            ->insert($delayedJobsTable->getSchema()->columns())
-            ->modifier('IGNORE')
-            ->values($selectQuery)
-            ->execute();
+            $insertQuery = $this->Archive->query();
+            $insertQuery->insert($columns)
+                ->modifier('IGNORE')
+                ->values($toArchiveQuery)
+                ->execute();
 
-        Log::debug('Jobs archived. Starting delete.');
-        $delayedJobsTable->deleteAll(['status IN' => [Job::STATUS_BURIED, Job::STATUS_SUCCESS]]);
-        Log::debug('Jobs deleted.');
+            Log::debug('Jobs archived. Starting delete.');
+            $clonedQuery->select(['id']);
+            $this->DelayedJobs->deleteAll(['id IN' => $clonedQuery->all()->extract('id')->toArray()]);
+            Log::debug('Jobs deleted.');
+        }
 
         if (Configure::read('DelayedJobs.archive.timeLimit')) {
             $time = new Time('-' . Configure::read('DelayedJobs.archive.timeLimit'));
             Log::debug('Cleaning archive. All jobs older than ' . $time);
-            $archiveTable->deleteAll([
-                'created <=' => $time,
-            ]);
+
+            $pageNumber = 1;
+            foreach ($this->getJobsToDelete($time) as $toDeleteQuery) {
+                Log::debug('Deleting page ' . $pageNumber++);
+
+                $this->Archive->deleteAll(
+                    [
+                        'id IN' => $toDeleteQuery->all()
+                            ->extract('id')
+                            ->toArray(),
+                    ]
+                );
+            }
+
             Log::debug('Archive cleaned.');
         }
 
